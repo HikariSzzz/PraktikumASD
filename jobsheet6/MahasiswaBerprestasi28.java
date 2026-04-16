7a3e87556065ebfbef9b2c368a8e2c9279f2727e
HikariSzzz <varellnashwaalifiowijaya@gmail.com>
2026-04-16 09:54:55 +0700

    6

---
+++ jobsheet6/MahasiswaBerprestasi28.java
@@ -0,0 +1,69 @@
+public class MahasiswaBerprestasi28 {
+
+    Mahasiswa28[] listMhs = new Mahasiswa28[5];
+    int idx;
+
+    void tambah(Mahasiswa28 mhs) {
+        if (idx < listMhs.length) {
+            listMhs[idx] = mhs;
+            idx++;
+        } else {
+            System.out.println("Data sudah penuh!");
+        }
+    }
+
+    void tampil() {
+        for (int i = 0; i < idx; i++) {
+            System.out.println("Nama: " + listMhs[i].nama);
+            System.out.println("NIM: " + listMhs[i].nim);
+            System.out.println("Kelas: " + listMhs[i].kelas);
+            System.out.println("IPK: " + listMhs[i].ipk);
+            System.out.println("-------------------------------");
+        }
+    }
+
+    int sequentialSearching(double cari) {
+        int posisi = -1;
+        for (int j = 0; j < listMhs.length; j++) {
+            if (listMhs[j].ipk == cari) {
+                posisi = j;
+                break;
+            }
+        }
+        return posisi;
+    }
+
+    void tampilPosisi(double x, int pos) {
+        if (pos != -1) {
+            System.out.println("data mahasiswa dengan IPK :" + x + "  ditemukan pada indeks " + pos);
+        } else {
+            System.out.println("data " + x + " tidak ditemukan");
+        }
+    }
+
+    void tampilDataSearch(double x, int pos) {
+        if (pos != -1) {
+            System.out.println("nim\t : " + listMhs[pos].nim);
+            System.out.println("nama\t : " + listMhs[pos].nama);
+            System.out.println("kelas\t : " + listMhs[pos].kelas);
+            System.out.println("ipk\t : " + x);
+        } else {
+            System.out.println("Data mahasiswa dengan IPK " + x + " tidak ditemukan");
+        }
+    }
+
+    int findBinarySearch(double cari, int left, int right) {
+        int mid;
+        if (right >= left) {
+            mid = (left + right) / 2;
+            if (cari == listMhs[mid].ipk) {
+                return (mid);
+            } else if (listMhs[mid].ipk > cari) {
+                return findBinarySearch(cari, left, mid - 1);
+            } else {
+                return findBinarySearch(cari, mid + 1, right);
+            }
+        }
+        return -1;
+    }
+}
